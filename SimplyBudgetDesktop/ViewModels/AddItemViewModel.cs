885f68bf6c55ecc54118a3238fe0f84f7317e69e
Kevin Bost <kevin@intellitect.com>
2020-09-29 15:02:24 -0700

    Showing add flyout

---
+++ SimplyBudgetDesktop/ViewModels/AddItemViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using System;
+using System.Windows.Input;
+
+namespace SimplyBudget.ViewModels
+{
+    public class AddItemViewModel : ObservableObject
+    {
+        public ICommand SubmitCommand { get; }
+        public IMessenger Messenger { get; }
+
+        public AddItemViewModel(IMessenger messenger)
+        {
+            SubmitCommand = new RelayCommand(OnSubmit);
+            Messenger = messenger ?? throw new ArgumentNullException(nameof(messenger));
+        }
+
+        private void OnSubmit()
+        {
+            Messenger.Send(new ItemAddedMessage());
+        }
+
+        public class ItemAddedMessage { }
+    }
+}
--- SimplyBudgetDesktop/ViewModels/MainWindowViewModel.cs
+++ SimplyBudgetDesktop/ViewModels/MainWindowViewModel.cs
@@ -1,25 +1,56 @@
 ﻿using AutoDI;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using Microsoft.Toolkit.Mvvm.Messaging;
 using SimplyBudget.ViewModels.MainWindow;
 using SimplyBudgetShared.Data;
 using System;
+using System.Windows.Input;
 
 namespace SimplyBudget.ViewModels
 {
-    public class MainWindowViewModel : ObservableObject
+    public class MainWindowViewModel : ObservableObject,
+        IRecipient<AddItemViewModel.ItemAddedMessage>
     {
         public BudgetViewModel Budget { get; }
 
         public HistoryViewModel History { get; }
 
-        public MainWindowViewModel([Dependency]IMessenger messenger = null, [Dependency]BudgetContext context = null)
+        private AddItemViewModel _addItem;
+        public AddItemViewModel AddItem
         {
-            Budget = new BudgetViewModel(messenger ?? throw new ArgumentNullException(nameof(messenger)));
+            get => _addItem;
+            set => SetProperty(ref _addItem, value);
+        }
+
+        public ICommand ShowAddCommand { get; }
+        public IMessenger Messenger { get; }
+
+        public MainWindowViewModel(
+            [Dependency]IMessenger messenger = null, 
+            [Dependency]BudgetContext context = null)
+        {
+            ShowAddCommand = new RelayCommand(OnShowAdd);
+
+            Messenger = messenger ?? throw new ArgumentNullException(nameof(messenger));
+
+            Budget = new BudgetViewModel(messenger);
             History = new HistoryViewModel(context);
 
             Budget.LoadItemsAsync();
             History.LoadItemsAsync();
+
+            messenger.Register(this);
+        }
+
+        private void OnShowAdd()
+        {
+            AddItem = new AddItemViewModel(Messenger);
+        }
+
+        public void Receive(AddItemViewModel.ItemAddedMessage message)
+        {
+            AddItem = null;
         }
     }
 }
