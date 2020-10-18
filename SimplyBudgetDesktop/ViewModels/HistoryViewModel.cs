dc148d321756ab0b531f43aa2e695adadaadf43c
Kevin Bost <kitokeboo@gmail.com>
2020-10-18 13:09:32 -0700

    Added filtering to history view

--- SimplyBudgetDesktop/ViewModels/HistoryViewModel.cs
+++ SimplyBudgetDesktop/ViewModels/HistoryViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Toolkit.Mvvm.Input;
 using Microsoft.Toolkit.Mvvm.Messaging;
 using SimplyBudget.Messaging;
 using SimplyBudgetShared.Data;
@@ -6,9 +7,12 @@ using SimplyBudgetShared.Events;
 using SimplyBudgetShared.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SimplyBudget.ViewModels.MainWindow
 {
@@ -24,13 +28,43 @@ namespace SimplyBudget.ViewModels.MainWindow
         public BudgetContext Context { get; }
         public ICurrentMonth CurrentMonth { get; }
 
+        public IRelayCommand AddFilterCommand { get; }
+        public ICommand RemoveFilterCommand { get; }
+
         public ICollectionView HistoryView => _view;
+
+        public ObservableCollection<ExpenseCategory> ExpenseCategories { get; }
+            = new ObservableCollection<ExpenseCategory>();
+
+        public ObservableCollection<ExpenseCategory> FilterCategories { get; }
+            = new ObservableCollection<ExpenseCategory>();
+
+        private ExpenseCategory _selectedCategory;
+        public ExpenseCategory SelectedCategory
+        {
+            get => _selectedCategory;
+            set
+            {
+                if (SetProperty(ref _selectedCategory, value))
+                {
+                    AddFilterCommand.NotifyCanExecuteChanged();
+                }
+            }
+        }
 
         public HistoryViewModel(BudgetContext context, IMessenger messenger, ICurrentMonth currentMonth)
         {
             Context = context ?? throw new ArgumentNullException(nameof(context));
             CurrentMonth = currentMonth ?? throw new ArgumentNullException(nameof(currentMonth));
             Sort(nameof(BudgetHistoryViewModel.Date), ListSortDirection.Descending);
+
+            ExpenseCategories.AddRange(context.ExpenseCategories);
+
+            AddFilterCommand = new RelayCommand<ExpenseCategory>(OnAddFilter, x => x != null);
+            RemoveFilterCommand = new RelayCommand<ExpenseCategory>(
+                x => FilterCategories.Remove(x), x => x != null);
+
+            FilterCategories.CollectionChanged += FilterCategories_CollectionChanged;
 
             messenger.Register<TransactionEvent>(this);
             messenger.Register<TransactionItemEvent>(this);
@@ -40,20 +74,62 @@ namespace SimplyBudget.ViewModels.MainWindow
             messenger.Register<CurrentMonthChanged>(this);
         }
 
+        private void OnAddFilter(ExpenseCategory category)
+        {
+            if (!FilterCategories.Contains(category))
+            {
+                FilterCategories.Add(category);
+            }
+            SelectedCategory = null;
+        }
+
+        private void FilterCategories_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+            => LoadItemsAsync();
+
         protected override async IAsyncEnumerable<BudgetHistoryViewModel> GetItems()
         {
             var oldestTime = CurrentMonth.CurrenMonth.AddMonths(-2).StartOfMonth();
 
-            await foreach(var item in Context.Incomes.Where(x => x.Date >= oldestTime).AsAsyncEnumerable())
+            var categoryList = new List<int>();
+            if (FilterCategories.Any())
+            {
+                categoryList.AddRange(FilterCategories.Select(x => x.ID));
+            }
+            
+            var incomeQuery = Context.Incomes.Where(x => x.Date >= oldestTime);
+            if (categoryList.Any())
+            {
+
+                incomeQuery = from income in incomeQuery
+                              join item in Context.IncomeItems on income.ID equals item.IncomeID
+                              where categoryList.Contains(item.ExpenseCategoryID)
+                              select income;
+            }
+            await foreach(var item in incomeQuery.AsAsyncEnumerable())
             {
                 yield return BudgetHistoryViewModel.Create(item);
             }
-            await foreach (var item in Context.Transactions.Where(x => x.Date >= oldestTime).AsAsyncEnumerable())
+
+            var transactionQuery = Context.Transactions.Where(x => x.Date >= oldestTime);
+            if (categoryList.Any())
+            {
+                transactionQuery = from transaction in transactionQuery
+                                   join item in Context.TransactionItems on transaction.ID equals item.TransactionID
+                                   where categoryList.Contains(item.ExpenseCategoryID)
+                                   select transaction;
+            }
+            await foreach (var item in transactionQuery.AsAsyncEnumerable())
             {
                 int total = Context.TransactionItems.Where(x => x.TransactionID == item.ID).Select(x => x.Amount).Sum();
                 yield return BudgetHistoryViewModel.Create(item, total);
             }
-            await foreach(var item in Context.Transfers.Where(x => x.Date >= oldestTime).AsAsyncEnumerable())
+
+            var transferQuery = Context.Transfers.Where(x => x.Date >= oldestTime);
+            if (categoryList.Any())
+            {
+                transferQuery = transferQuery.Where(x => categoryList.Contains(x.FromExpenseCategoryID) || categoryList.Contains(x.ToExpenseCategoryID));
+            }
+            await foreach(var item in transferQuery.AsAsyncEnumerable())
             {
                 var from = await Context.FindAsync<ExpenseCategory>(item.FromExpenseCategoryID);
                 var to = await Context.FindAsync<ExpenseCategory>(item.ToExpenseCategoryID);
