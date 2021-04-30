605a2b58171bbd33d6ef659242c50bc771e6f00b
Kevin Bost <kitokeboo@gmail.com>
2021-04-30 00:27:46 -0700

    Settings can now open settings directory
    Fixed name of VM property to be storage location

--- SimplyBudgetDesktop/ViewModels/SettingsViewModel.cs
+++ SimplyBudgetDesktop/ViewModels/SettingsViewModel.cs
@@ -4,6 +4,8 @@ using Microsoft.Toolkit.Mvvm.Messaging;
 using SimplyBudget.Messaging;
 using SimplyBudget.Properties;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Windows.Input;
 
 namespace SimplyBudget.ViewModels
@@ -12,29 +14,37 @@ namespace SimplyBudget.ViewModels
     {
         private IMessenger Messenger { get; }
         public ICommand SaveCommand { get; }
+        public ICommand OpenFolderCommand { get; }
 
         public SettingsViewModel(IMessenger messenger)
         {
             SaveCommand = new RelayCommand(OnSaveCommand);
+            OpenFolderCommand = new RelayCommand(OpenFolder);
             Messenger = messenger ?? throw new ArgumentNullException(nameof(messenger));
         }
 
         private void OnSaveCommand()
         {
-            if (string.Equals(ConnectionString, Settings.Default.StorageLocation, StringComparison.Ordinal))
+            if (string.Equals(StorageLocation, Settings.Default.StorageLocation, StringComparison.Ordinal))
             {
                 return;
             }
-            Settings.Default.StorageLocation = ConnectionString;
+            Settings.Default.StorageLocation = StorageLocation;
             Settings.Default.Save();
             Messenger.Send(new StorageLocationChanged());
         }
 
-        private string _ConnectionString = Settings.Default.StorageLocation;
-        public string ConnectionString
+        private string _StorageLocation = Settings.Default.StorageLocation;
+        public string StorageLocation
         {
-            get => _ConnectionString;
-            set => SetProperty(ref _ConnectionString, value);
+            get => _StorageLocation;
+            set => SetProperty(ref _StorageLocation, value);
+        }
+
+        private void OpenFolder()
+        {
+            string targetPath = Path.GetFullPath(Environment.ExpandEnvironmentVariables(StorageLocation));
+            Process.Start("explorer.exe", targetPath);
         }
     }
 }
