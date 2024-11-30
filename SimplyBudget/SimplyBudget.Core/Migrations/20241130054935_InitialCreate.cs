b33731e8c80c1bb1cb9ae15f74e57a05cd528d96
Kevin Bost <kitokeboo@gmail.com>
2024-11-29 21:50:04 -0800

    Adding local ef with migration

---
+++ SimplyBudget/SimplyBudget.Core/Migrations/20241130054935_InitialCreate.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimplyBudget.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    ValidatedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsDefault = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseCategoryItems",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseCategoryItems", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Metadatas",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Key = table.Column<string>(type: "TEXT", nullable: true),
+                    Value = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Metadatas", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseCategories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CategoryName = table.Column<string>(type: "TEXT", nullable: true),
+                    AccountID = table.Column<int>(type: "INTEGER", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    BudgetedPercentage = table.Column<int>(type: "INTEGER", nullable: false),
+                    BudgetedAmount = table.Column<int>(type: "INTEGER", nullable: false),
+                    CurrentBalance = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cap = table.Column<int>(type: "INTEGER", nullable: true),
+                    IsHidden = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseCategories", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ExpenseCategories_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalTable: "Accounts",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseCategoryItemDetails",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ExpenseCategoryItemId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ExpenseCategoryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Amount = table.Column<int>(type: "INTEGER", nullable: false),
+                    IgnoreBudget = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseCategoryItemDetails", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ExpenseCategoryItemDetails_ExpenseCategories_ExpenseCategoryId",
+                        column: x => x.ExpenseCategoryId,
+                        principalTable: "ExpenseCategories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExpenseCategoryItemDetails_ExpenseCategoryItems_ExpenseCategoryItemId",
+                        column: x => x.ExpenseCategoryItemId,
+                        principalTable: "ExpenseCategoryItems",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseCategoryRules",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    RuleRegex = table.Column<string>(type: "TEXT", nullable: true),
+                    ExpenseCategoryID = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseCategoryRules", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ExpenseCategoryRules_ExpenseCategories_ExpenseCategoryID",
+                        column: x => x.ExpenseCategoryID,
+                        principalTable: "ExpenseCategories",
+                        principalColumn: "ID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_IsDefault",
+                table: "Accounts",
+                column: "IsDefault");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseCategories_AccountID",
+                table: "ExpenseCategories",
+                column: "AccountID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseCategories_CategoryName",
+                table: "ExpenseCategories",
+                column: "CategoryName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseCategoryItemDetails_ExpenseCategoryId",
+                table: "ExpenseCategoryItemDetails",
+                column: "ExpenseCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseCategoryItemDetails_ExpenseCategoryItemId",
+                table: "ExpenseCategoryItemDetails",
+                column: "ExpenseCategoryItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseCategoryRules_ExpenseCategoryID",
+                table: "ExpenseCategoryRules",
+                column: "ExpenseCategoryID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseCategoryItemDetails");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseCategoryRules");
+
+            migrationBuilder.DropTable(
+                name: "Metadatas");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseCategoryItems");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseCategories");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
