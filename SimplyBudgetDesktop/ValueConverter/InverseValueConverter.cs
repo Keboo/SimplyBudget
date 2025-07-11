7aaa367e4babaceba190522e443c37e4c3f48019
Kevin Bost <kitokeboo@gmail.com>
2025-07-10 23:30:30 -0700

    Adds inverse boolean to visibility converter
    Adds a converter to invert boolean values to visibility,
    allowing for easier control of UI element visibility based
    on boolean conditions.

---
+++ SimplyBudgetDesktop/ValueConverter/InverseValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SimplyBudget.ValueConverter;
+
+public class InverseBoolToVisConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool booleanValue)
+        {
+            return booleanValue ? Visibility.Collapsed : Visibility.Visible;
+        }
+        return DependencyProperty.UnsetValue;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is Visibility visibility)
+        {
+            return visibility != Visibility.Visible;
+        }
+        return DependencyProperty.UnsetValue;
+    }
+}
